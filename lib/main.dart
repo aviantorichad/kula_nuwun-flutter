144b54fcbdc9aba0d6ac699923b2fc75da1ac1b8
Richad Avianto <aviantorich@gmail.com>
2019-01-04 18:31:34 +0700

    yay sudah jadi startup name generator

--- lib/main.dart
+++ lib/main.dart
@@ -7,15 +7,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Kula Nuwun',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Sugeng Rawuh'),
-        ),
-        body: Center(
-          child: RandomWords(),
-        ),
-      ),
+      title: 'Startup Name Generator',
+      home: RandomWords(),
     );
   }
 }
